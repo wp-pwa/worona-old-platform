89ced79694a01b762b3ac3b095e75414cb0344e3
Luis Herranz <luisherranz@gmail.com>
2015-10-06 17:58:58 +0200

    change hooks api package

--- packages/hooks-api/package.js
+++ packages/hooks-api/package.js
@@ -2,19 +2,17 @@ Package.describe({
   name: 'worona:hooks-api',
   version: '1.0.0',
   summary: 'Hooks API for Worona apps. Based on WP Hooks.',
-  git: 'https://github.com/worona/worona-app/',
-  documentation: 'README.md'
+  git: 'https://github.com/worona/worona/',
+  documentation: 'README.md',
 });
 
-Package.onUse(function (api) {
-  api.versionsFrom('1.1.0.3');
-
+Package.onUse(function(api) {
+  api.versionsFrom('1.2');
   api.use('worona:external-libraries@1.0.0');
-
   api.addFiles('lib/hooks-api.js');
 });
 
-Package.onTest(function (api) {
+Package.onTest(function(api) {
   api.use('tinytest');
   api.use('worona:external-libraries@1.0.0');
   api.use('worona:hooks-api');
