d0e6b4d66e36d7f7a0eb76ac144e6c9207c0eda2
Luis Herranz <luisherranz@gmail.com>
2015-10-13 15:29:42 +0200

    refactoring of router using app state modify

--- packages/dashboard-default-state/lib/default-state.js
+++ packages/dashboard-default-state/lib/default-state.js
@@ -1,21 +1,7 @@
-// Pages state
-AppState.set('isHome', false);
-AppState.set('isProfile', false);
-AppState.set('isLogin', false);
-AppState.set('isCreateYourFirstApp', false);
-AppState.set('isGeneralSettings', false);
-
-// Url state
-AppState.set('homeUrl', '/');
-AppState.set('profileUrl', '/profile');
-AppState.set('loginUrl', '/login-or-sign-up');
-AppState.set('createYourFirstAppUrl', '/create-your-first-app');
-
 // Theme
 AppState.set('theme', false);
 
 // Login state
-AppState.set('redirectAfterLogin', AppState.get('homeUrl'));
 AppState.set('lastEmailEnteredInLogin', '');
 AppState.set('loginNameGuess', 'John');
 AppState.set('loginError', false);
--- packages/dashboard-router/lib/router.js
+++ packages/dashboard-router/lib/router.js
@@ -1,51 +1,61 @@
+// Add your routes here. For each route, some things get registered.
+// For example, for route SomePlace with path /some-place:
+// - a 'isSomePlace' helper (true or false).
+// - a 'SomePlaceUrl' helper with '/some-place' url.
+// - an action 'SHOW_SOME_PLACE' dispatched when '/some-place' is visited.
+let routes = {
+  Home: '/',
+  Login: '/login',
+  Profile: '/profile',
+  CreateYourFirstApp: '/create-your-first-app'
+};
 
-var dashboard = FlowRouter.group({
-  name: 'dashboard',
-  triggersEnter: [function(context, redirect) {
+// Get action type for route. For example, SHOW_HOME for Home or
+// SHOW_CREATE_YOUR_FIRST_APP for CreateYourFirstApp.
+let getActionType = function(route) {
+  route = s.replaceAll(route, ':.+?/', '_');
+  route = s.replaceAll(route, '/', '');
+  route = 'SHOW_' + s.humanize(route).toUpperCase();
+  return s.replaceAll(route, ' ', '_');
+};
+
+
+// Register all the stuff needed for each route.
+for (var route in routes) {
+  let actionType = getActionType(route);
+
+  // Create state for urls. For example, HomeUrl or LoginUrl
+  let stateUrl = route + 'Url';
+  let url = routes[route];
+  AppState.set(stateUrl, url);
+
+  // Create state for is. For example, isHome or isLogin
+  let isState = 'is' + route;
+  AppState.modify(isState, (action, state = false) => {
+    return action.type.startsWith(actionType) ? true : false;
+  });
+
+  // Dispatch an action when the actionType is visited.
+  FlowRouter.route(url, {
+    action(params) {
+      Dispatcher.dispatch(actionType, params);
+    }
+  });
+}
+
+// Create trigger to send people to login if they aren't logged.
+FlowRouter.triggers.enter([
+  (context, redirect) => {
     if (!Meteor.userId()) {
       AppState.set('redirectAfterLogin', FlowRouter.current().path);
       redirect(AppState.get('loginUrl'));
     }
-  }]
-});
-
-FlowRouter.route(AppState.get('loginUrl'), {
-  action: function() {
-    Dispatcher.dispatch('SHOW_LOGIN');
-  },
-  triggersEnter: function(context, redirect) {
-    if (Meteor.userId()) {
-      redirect(AppState.get('redirectAfterLogin'));
-    }
   }
-});
+]);
 
-FlowRouter.route(AppState.get('createYourFirstAppUrl'), {
-  action: function() {
-    Dispatcher.dispatch('SHOW_CREATE_YOUR_FIRST_APP');
-  }
-});
-
-dashboard.route(AppState.get('homeUrl'), {
-  action: function() {
-    Dispatcher.dispatch('SHOW_HOME');
-  },
-});
-
-dashboard.route(AppState.get('profileUrl'), {
-  action: function() {
-    Dispatcher.dispatch('SHOW_PROFILE');
-  },
-});
-
-dashboard.route('/app/:id', {
+// Redirect to general settings when seeing an app.
+FlowRouter.route('/app/:id', {
   triggersEnter: [function(context, redirect) {
     redirect(context.path + '/general-settings');
-  },],
-});
-
-dashboard.route('/app/:id/general-settings', {
-  action: function(params) {
-    Dispatcher.dispatch('SHOW_APP_GENERAL_SETTINGS', { id: params.id });
-  },
+  }]
 });
