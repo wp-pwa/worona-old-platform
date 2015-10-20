cbdf5b98d7dba758ede596bab73c951b1f536962
Luis Herranz <luisherranz@gmail.com>
2015-10-20 11:09:51 +0200

    finish redirections

--- packages/dashboard-router/lib/redirections.js
+++ packages/dashboard-router/lib/redirections.js
@@ -1,12 +1,3 @@
-// Create trigger to send people to login if they aren't logged.
-// FlowRouter.triggers.enter([
-//   (context, redirect) => {
-//     if (!Meteor.userId()) {
-//       Dispatcher.dispatch('SHOW_LOGIN');
-//     }
-//   }
-// ], { except: ['Login'] });
-
 FluxRouter.redirection(action => {
   if ((action.type.startsWith('SHOW_') && (!Meteor.userId()))) {
     return 'SHOW_LOGIN';
@@ -14,10 +5,7 @@ FluxRouter.redirection(action => {
 });
 
 FluxRouter.redirection(action => {
-  switch (action.type) {
-    case 'SHOW_LOGIN':
-      if (Meteor.userId())
-        return 'SHOW_CREATE_YOUR_FIRST_APP';
-      break;
+  if ((action.type === 'SHOW_LOGIN') && (Meteor.userId())) {
+    return 'SHOW_CREATE_YOUR_FIRST_APP';
   }
 });
